7ccb5d25ca8caeb74e10d894aec6b23f2f37666a
Alan Burić <134159250+AlanBuric@users.noreply.github.com>
2025-02-26 22:51:29 +0100

    refactor: inline application endpoint

--- src/application.ts
+++ src/application.ts
@@ -1,12 +1,11 @@
-import express, {json, urlencoded} from "express";
+import express, {json, Router, urlencoded} from "express";
 import SessionRouter from "./routes/session/router.js";
 import handleError from "./routes/middleware/error-handler.js";
-import 'dotenv/config'
+import 'dotenv/config';
 import OrganizationRouter from "./routes/organization/router.js";
 
 const application = express()
-    .use("/api/v1", json(), urlencoded(), SessionRouter)
-    .use("/api/v1/organization", OrganizationRouter)
+    .use("/api/v1", json(), urlencoded(), SessionRouter, Router().use("/organizations", OrganizationRouter))
     .use(handleError);
 
 export default application;
